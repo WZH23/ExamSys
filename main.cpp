d7a75ca3cfc616d270b39ea5c2f578534aa1d7a1
王子菡 <245599624@qq.com>
2024-12-12 20:42:52 +0800

    修改点击关闭按钮，后台进程不退出的问题

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include "logindialog.h"
-#include "examdialog.h"
+//#include "examdialog.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     LoginDialog w;
-    w.exec();
+    auto res1 = w.exec();
+    if(res1 == QDialog::Rejected)
+        exit(0);
     return a.exec();
-//    ExamDialog e;
-//    e.show();
-
 }
